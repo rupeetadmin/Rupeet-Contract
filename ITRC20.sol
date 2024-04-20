a596a0397985eaf06b7054a3170777912f6cc84d
rupeetadmin <167621217+rupeetadmin@users.noreply.github.com>
2024-04-20 17:23:49 +0530

    Create ITRC20.sol

---
+++ ITRC20.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+/**
+ * @title TRC20 interface (compatible with ERC20 interface)
+ * @dev see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ */
+interface ITRC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address who) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+    external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value)
+    external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value)
+    external returns (bool);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    event Burn(
+        address indexed burner,
+        uint256 value
+    );
+
+    event Mint(
+        address indexed minter,
+        uint256 value
+    );
+}
